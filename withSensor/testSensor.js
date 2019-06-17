7950c1a5ad5243abaf15da59500142235ebdf4ba
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2019-06-17 15:21:17 -0400

    Update testSensor.js

--- withSensor/testSensor.js
+++ withSensor/testSensor.js
@@ -1,1 +1,19 @@
+var gpio = require('pi-gpio');
+gpio.setDirection(11, "input");
 
+
+function testSensor() {
+    gpio.read(11, function (err, value) {
+        if (err) {
+            throw err;
+        }
+        if (value === 0) {
+            console.log("The door is open")
+        }
+        else {
+            console.log("The door is closed")
+        }
+    });
+}
+
+setInterval(testSensor, 10000)
