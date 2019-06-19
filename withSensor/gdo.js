1a21313e32eba39b76510f079c6c6bd899ada8aa
tpeterson66 <35910834+tpeterson66@users.noreply.github.com>
2019-06-19 17:38:03 -0400

    Update gdo.js

--- withSensor/gdo.js
+++ withSensor/gdo.js
@@ -2,72 +2,57 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var gpio = require('pi-gpio');
+var rpio = require('rpio');
 
-gpio.setDirection(7, "output");
-gpio.setDirection(11, "input");
+// Input sensor for the door
+rpio.open(11, rpio.INPUT);
+rpio.open(7, rpio.OUTPUT, rpio.HIGH);
 
 app.set('view engine', 'ejs');
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//console.log(path.join(__dirname, 'public'));
-
 app.get('/', function (req, res) {
-    gpio.read(11, function (err, value) {
-        if (err) throw err;
-        if (value === 0) {
-            res.render('index', { status: "Garage Door Opener is Ready - Door is Open" })
-        } else {
-            res.render('index', { status: "Garage Door Opener is Ready - Door is Closed" })
-        }
-    })
+    value = rpio.read(11);
+    if (value === 0) {
+        //door is open
+        return res.render('index', { status: "The garage door is open" });
+    } else {
+        return res.render('index', { status: "The garage door is closed" });
+    }
 });
 
-function cleanUp() {
-    gpio.write(7, true, function (err) {
-        if (err) throw err;
-        //console.log('Trigger relay off');
-    });
+function triggerDoor() {
+    rpio.write(7, rpio.LOW)
+    rpio.sleep(.5);
+    rpio.write(7, rpio.HIGH)
 }
 
 app.post('/api/close', function (req, res) {
     //Check the status of the door
-    gpio.read(11, function (err, value) {
-        if (err) throw err;
-        if (value === 0) {
-            console.log("Door is open, closing the door.");
-            gpio.write(7, false, function (err) {
-                if (err) throw err;
-                console.log('Trigger relay on');
-                return res.render('index', { status: "Garage Door Is Closing" });
-            });
-            setTimeout(cleanUp, 500);
-        } else {
-            return res.render('index', { status: "Garage Door Is Already Closed..." });
-        }
-    });
+    value = rpio.read(11);
+    if (value === 0) {
+        //door is open
+        triggerDoor();
+        return res.render('index', { status: "Closing door" });
+    } else {
+        return res.render('index', { status: "Door is already closed..." });
+    }
 });
 
 app.post('/api/open', function (req, res) {
     //Check the status of the door
-    gpio.read(11, function (err, value) {
-        if (err) throw err;
-        if (value === 1) {
-            console.log("Door is closed, opening the door.");
-            gpio.write(7, false, function (err) {
-                if (err) throw err;
-                console.log('Trigger relay on');
-                return res.render('index', { status: "Garage Door Is Opening" });
-            });
-            setTimeout(cleanUp, 500);
-        } else {
-            return res.render('index', { status: "Garage Door Is Already Open..." });
-        }
-    });
+    value = rpio.read(11);
+    if (value === 0) {
+        //door is open
+        return res.render('index', { status: "Door is already open..." });
+    } else {
+        triggerDoor();
+        return res.render('index', { status: "Closing Door" });
+    }
 });
 
 
-app.listen(3001, function () {
+app.listen(3000, function () {
     console.log('Garage Door Opener Started  on Port: 3000!')
 })
